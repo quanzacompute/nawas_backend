fdd8c43bcd34c6b3200446efa753ee3df7be8a53
roys <roys@quanza.net>
2024-04-30 11:49:34 +0200

    setup tenant with a couple of endpoints

--- src/resources/tenant.py
+++ src/resources/tenant.py
@@ -2,56 +2,91 @@ from src.db.connection import DBConnection
 
 from flask import jsonify, request
 from flask_restful import Resource, fields, marshal_with, reqparse
-   
-resource_fields = {
+
+db = DBConnection.get_connection()
+
+class Tenant(db.Model):
+    ## metadata
+    __tablename__ = 'tenant'
+
+    ## columnm
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    ##TODO: add more fields if required
+
+
+tenant_fields = {
     'id': fields.Integer,
     'name': fields.String,
 }
 
-
-class Tenant():
-    name = None
-    id = None
-    
-    def __init__(self, id):
-        self.id = id
-
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-
+## retrieve a list of all tenants
 class TenantList(Resource):
+    @marshal_with(tenant_fields)
     def get(self):
-        return_list = []
-        cursor = DBConnection.get_cursor()
-        cursor.execute("SELECT id, name from tenants;")
-        tenants = cursor.fetchall()
-        for t in tenants:
-            return_list.append({
-                "id": t[0],
-                "name": t[1]
-            })
+        tenants = Tenant.query.all()
+        return tenants, 200
 
+## get, update or delete tenants based of id
 class TenantById(Resource):
-
-    @marshal_with(resource_fields, envelope='resource')
+    ## GET
+    @marshal_with(tenant_fields, envelope='resource')
     def get(self, tenant_id):
-        cursor = DBConnection.get_cursor()
-        cursor.execute("SELECT id, name from tenants where id={}".format(tenant_id))
+        tenant = Tenant.query.get(tenant_id)
+        if tenant:
+            return tenant, 200
+        else:
+            return {'error': 'Tenant not found'}, 404
+
+    ## UPDATE
+    @marshal_with(tenant_fields, envelope='resource')
+    def put(self, tenant_id):
+        args = request.get_json()
+        tenant = Tenant.query.get(tenant_id)
+
+        if tenant:
+            tenant.id = args.get('id', tenant.id)
+            tenant.name = args.get('name', tenant.name)
+            db.session.commit()
+            return tenant, 200
+        else:
+            return {'error', 'Tenant not found'}, 404
+            
+
+    ## DELETE
+    def delete(self, tenant_id):
+        tenant = Tenant.query.get(tenant_id)
         
-        for t in cursor.fetchall():
-            return Tenant(t["id"], t["name"])
+        if tenant:
+            db.session.delete(tenant)
+            db.session.commit()
+            return {'message': 'Tenant deleted successfully'}, 200
+        else:
+            return {'error', 'Tenant not found'}, 404
 
-    
-    @marshal_with(resource_fields, envelope='resource')
-    def post(self, tenant_id):
-       cursor = DBConnection.get_cursor()
-       args = request.get_json()
-       tenant = 
-
+## get or add tenants based of name
 def TenantByName(Resource):
-
-    @marshal_with(resource_fields, envelope='resource')
+    ## GET
+    @marshal_with(tenant_fields)
     def get(self, tenant_name):
+        tenant = Tenant.query.get(name=tenant_name).first()
+        if tenant:
+            return tenant, 200
+        else:
+            return {'error': 'Tenant not found'}, 404
+    
+    ## CREATE
+    @marshal_with(tenant_fields)
+    def post(self, tenant_name):args = request.get_json()
+        tenant = Tenant.query.filter_by(name=tenant_name).first()
+        if tenant:
+            return {'error': 'Tenant already exists'}, 409
+        else:
+            # Creating a new tenant
+            new_tenant = Tenant(name=tenant_name)
+            db.session.add(new_tenant)
+            db.session.commit()
+            return new_tenant, 201
+    
 
     
