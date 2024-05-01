88ea1374b28c72cc6c44f8c48126397e6eace735
roys <roys@quanza.net>
2024-05-01 15:00:13 +0200

    prepared app.py for DEV situation

--- flask/app.py
+++ flask/app.py
@@ -6,21 +6,24 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_continuum import make_versioned
 
 ## method to generate connection uri
-def build_uri():
-    ## Read secrets
-    DB_HOST="mysql"
-    DB_NAME="nawas"
-    DB_USER="api"
-    DB_PASSWORD_FILE="/run/secrets/db_password"
-    DB_PASSWORD = None
-
-    with open(DB_PASSWORD_FILE) as f:
-        DB_PASSWORD = f.readlines()
-
-    if DB_PASSWORD is None:
-        raise DBPasswordNotFoundException("db_password was not found in the expected location (/run/secrets/db_password)")
-
-    return "mysql+mysqlconnector://{}:{}@{}/{}".format(DB_USER,DB_PASSWORD,DB_HOST,DB_NAME)
+def get_uri(env):
+    if env=="DEV":
+        return 'sqlite:///test.db'
+    elif env=="PROD":
+        ## Read secrets
+        DB_HOST="mysql"
+        DB_NAME="nawas"
+        DB_USER="api"
+        DB_PASSWORD_FILE="/run/secrets/db_password"
+        DB_PASSWORD = None
+    
+        with open(DB_PASSWORD_FILE) as f:
+            DB_PASSWORD = f.readlines()
+    
+        if DB_PASSWORD is None:
+            raise DBPasswordNotFoundException("db_password was not found in the expected location (/run/secrets/db_password)")
+    
+        return "mysql+mysqlconnector://{}:{}@{}/{}".format(DB_USER,DB_PASSWORD,DB_HOST,DB_NAME)
 
 ## custom error for missing password in secrets location
 class DBPasswordNotFoundException(Exception):
@@ -30,7 +33,7 @@ class DBPasswordNotFoundException(Exception):
 
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = build_uri()
+app.config['SQLALCHEMY_DATABASE_URI'] = get_uri()
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 make_versioned
 
