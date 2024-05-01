255bc3ad4a22dad7c7026f1893b3b956d2990247
roys <roys@quanza.net>
2024-05-01 14:56:01 +0200

    reshuffle test scripts

--- flask/tests/__init__.py
+++ flask/tests/__init__.py
@@ -1,18 +1,16 @@
 
 import unittest
 
-from app import app, db
-from test import *
+import test_tenant
+import test_asn
+import test_prefix
 
-class TestAPI(unittest.TestSuite):
-
-    @classmethod
-    def suite(cls):
-        suite = unittest.TestSuite()
-        suite.addTest(unittest.TestLoader.loadTestsFromTestCase(test_tenant.TestTenants))
-        suite.addTest(unittest.TestLoader.loadTestsFromTestCase(test_asn.TestASNs))
-        suite.addTest(unittest.TestLoader.loadTestsFromTestCase(test_prefix.TestPrefixes))
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.TestLoader.loadTestsFromTestCase(test_tenant.TestTenants))
+    suite.addTest(unittest.TestLoader.loadTestsFromTestCase(test_asn.TestASNs))
+    suite.addTest(unittest.TestLoader.loadTestsFromTestCase(test_prefix.TestPrefixes))
 
 if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(TestAPI.suite())
+    unittest.TextTestRunner(verbosity=2).run(suite())
 
---
+++ flask/tests/run.py
@@ -0,0 +1,5 @@
+import unittest
+from tests import suite
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(suite())
