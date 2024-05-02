9b29feb23d9e5e9455714e140b8f04eec8a37322
roys <roys@quanza.net>
2024-05-02 13:10:37 +0200

    added some debug logging

--- flask/app.py
+++ flask/app.py
@@ -11,19 +11,31 @@ import conf
 
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.logger.debug("Starting application")
 api = Api(app)
+app.logger.debug("Adding api function")
 
+app.logger.debug("Loading config")
 if ( os.environ.get("FLASK_ENV") == "testing" ):
-    app.config.from_object(conf.TestConfig) 
+    app.config.from_object(conf.TestConfig)
+    app.logger.debug("loaded testing config...")
 elif ( os.environ.get("FLASK_ENV") == "production" ):
     app.config.from_object(conf.ProductionConfig)
+    app.logger.debug("loaded production config...")
+    app.logger.debug(app.config)
 
+
+app.logger.debug("Initialising database integration")
 db = SQLAlchemy(app)
 make_versioned
 
 from urls import loadUrls
+app.logger.debug("loading URLs")
 loadUrls(api)
 
 ## INIT
 if __name__ == "__main__":
-    app.run()
+#    app.run(debug=bool(os.environ.get("DEBUG")))
+    print("hello NAWAS")
+    app.run(debug=True)
